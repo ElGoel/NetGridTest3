823a933394edae5216be360aaea8d9c95bd19d4b
ElGoel <dazhkhors@gmail.com>
2022-09-01 16:48:45 -0500

    Prueba Tecnica Terminada de Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'night-swich';
+
+
+  lists: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+}
